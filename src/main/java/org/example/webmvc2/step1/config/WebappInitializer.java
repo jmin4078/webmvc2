5cea662173700715c93236693daa4509902e59e1
jmin4078 <wpalsrl8588@gmail.com>
2026-07-07 09:43:50 +0900

    Initial commit

---
+++ src/main/java/org/example/webmvc2/step1/config/WebappInitializer.java
@@ -0,0 +1,12 @@
+package org.example.webmvc2.step1.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+    }
+}
